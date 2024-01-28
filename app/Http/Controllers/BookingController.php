7500454a864930e80de3c44679de0f5ad5b4b24d
Alec Foster <alecfoster412@gmail.com>
2024-01-28 17:54:35 +0000

    Prevent appointments with same datetime

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -17,16 +17,31 @@ class BookingController extends Controller
 
     public function createBooking(Request $request): JsonResponse
     {
-        $booking = new Booking([
-            'name' => 'test',
-            'email' => 'test',
-            'phone_number' => 'test',
-            'vehicle_make_model' => 'test',
-            'booking_datetime' => new \DateTimeImmutable('2024-01-29'),
-        ]);
+        try {
+            $data = $request->all();
+
+            $booking = new Booking([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'phone_number' => $data['phone_number'],
+                'vehicle_make_model' => $data['vehicle_make_model'],
+                'booking_datetime' => new \DateTimeImmutable($data['booking_datetime']),
+            ]);
+
+            $existingBooking = Booking::firstWhere(['booking_datetime' => $data['booking_datetime']]);
+
+            if ($existingBooking !== null) {
+                throw new \Exception('Appointment already exists with this datetime.');
+            }
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 400);
+        }
 
         $booking->save();
 
-        return new JsonResponse($booking->get('id'));
+        return new JsonResponse([
+            'booking_id' => $booking->id,
+            'booking_datetime' => $booking->booking_datetime,
+        ]);
     }
 }
